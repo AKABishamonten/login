1ef309671229761016393260f3364f70e153cb5e
theerawatnam <theerawat.kan@hotmail.com>
2023-01-20 11:20:59 +0700

    update image

--- lib/profile.dart
+++ lib/profile.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_line_sdk/flutter_line_sdk.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:loginfuc/main.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -49,6 +50,8 @@ class _ProfilePageState extends State<ProfilePage> {
           ),
           (route) => false);
     });
+    final googleSignIn = GoogleSignIn();
+    await googleSignIn.disconnect();
     try {
       LineSDK.instance.setup("${1657836028}").then((_) {});
       await LineSDK.instance.logout();
